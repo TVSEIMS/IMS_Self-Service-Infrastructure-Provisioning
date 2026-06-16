b9f9bd51c4d7ea831704daa1875c984bc05044fe
TVSEIMS <karishmawork26@gmail.com>
2026-06-16 22:10:30 +0530

    Add AWS provider and EC2 instance resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,12 @@
+provider "aws" {
+  region = var.region
+}
 
+resource "aws_instance" "app" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = var.instance_name
+  }
+}
